f0a4f0a171018bfda72107a9cfc1c46032bd47d6
KidusGR <kidmen10@gmail.com>
2023-02-17 22:00:52 +0300

    0x1B. C - Sorting algorithms & Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,51 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * bubble_sort - sorts an array in ascending order
- * @array: array of ints to sort
+ * do_swap - change  positions in array
+ * @array: array list
+ * @pos1: position to change
+ * @pos2: position to change
+ *
+ * Return: array pointer
+ */
+int *do_swap(int *array, int pos1, int pos2)
+{
+	array[pos1] = array[pos1] ^ array[pos2];
+	array[pos2] = array[pos1] ^ array[pos2];
+	array[pos1] = array[pos1] ^ array[pos2];
+
+	return (array);
+}
+
+
+
+/**
+ * bubble_sort - order an array in ascenden form
+ * @array: array
  * @size: size of the array
+ *
+ * Return: none
  */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j;
-	int tmp;
+	size_t j;
+	unsigned long int c = 0;
+	int flag = 1;
 
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	while (size > 0 && flag)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		flag = 0;
+		for (j = 1; j < (size - c); j++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[j - 1] > array[j])
 			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				flag++;
+				do_swap(array, (j - 1), j);
 				print_array(array, size);
 			}
-		}
+		} c++;
 	}
 }
-
